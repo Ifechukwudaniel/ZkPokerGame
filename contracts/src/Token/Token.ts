d128bec0d1f8de989081d583b64ab3a0ef1e889a
Angry Coding <mailidanielifechukwu@gmail.com>
2023-07-04 17:20:05 +0100

    Token contract update

--- contracts/src/Token/Token.ts
+++ contracts/src/Token/Token.ts
@@ -8,6 +8,8 @@ import {
   PublicKey,
   Signature,
   UInt32,
+  AccountUpdate,
+  Int64,
 } from 'snarkyjs';
 
 const tokenSymbol = 'Poker';
@@ -76,5 +78,38 @@ export class Token extends SmartContract {
       to: receiverAddress,
       amount,
     });
+  }
+
+  // let a zkapp do anything, provided the token supply stays constant
+  @method approveUpdate(zkappUpdate: AccountUpdate) {
+    this.approve(zkappUpdate); // TODO is this secretly approving other changes?
+
+    // see if balance change is zero
+    let balanceChange = Int64.fromObject(zkappUpdate.body.balanceChange);
+    balanceChange.assertEquals(Int64.from(0));
+  }
+
+  // let a zkapp send tokens to someone, provided the token supply stays constant
+  @method approveUpdateAndSend(
+    zkappUpdate: AccountUpdate,
+    to: PublicKey,
+    amount: UInt64
+  ) {
+    this.approve(zkappUpdate); // TODO is this secretly approving other changes?
+
+    // see if balance change cancels the amount sent
+    let balanceChange = Int64.fromObject(zkappUpdate.body.balanceChange);
+    balanceChange.assertEquals(Int64.from(amount).neg());
+    // add same amount of tokens to the receiving address
+    this.token.mint({ address: to, amount });
+  }
+
+  @method getBalance(publicKey: PublicKey): UInt64 {
+    let accountUpdate = AccountUpdate.create(publicKey, this.token.id);
+    let balance = accountUpdate.account.balance.get();
+    accountUpdate.account.balance.assertEquals(
+      accountUpdate.account.balance.get()
+    );
+    return balance;
   }
 }
