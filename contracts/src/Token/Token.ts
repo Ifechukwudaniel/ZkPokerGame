b0acfa92db3269d770e302c275af28fdf63b1305
Angry Coding <mailidanielifechukwu@gmail.com>
2023-07-04 12:48:49 +0100

    Token

---
+++ contracts/src/Token/Token.ts
@@ -0,0 +1,80 @@
+import {
+  SmartContract,
+  state,
+  State,
+  method,
+  Permissions,
+  UInt64,
+  PublicKey,
+  Signature,
+  UInt32,
+} from 'snarkyjs';
+
+const tokenSymbol = 'Poker';
+
+export class Token extends SmartContract {
+  @state(UInt64) totalAmountInCirculation = State<UInt64>();
+  @state(UInt32) mintNonce = State<UInt32>();
+
+  deploy() {
+    super.deploy();
+
+    const permissionToEdit = Permissions.proof();
+
+    this.account.permissions.set({
+      ...Permissions.default(),
+      editState: permissionToEdit,
+      setTokenSymbol: permissionToEdit,
+      send: permissionToEdit,
+      receive: permissionToEdit,
+    });
+  }
+
+  @method init() {
+    super.init();
+    this.account.tokenSymbol.set(tokenSymbol);
+    this.totalAmountInCirculation.set(UInt64.zero);
+  }
+
+  @method mint(
+    receiverAddress: PublicKey,
+    amount: UInt64,
+    adminSignature: Signature
+  ) {
+    let totalAmountInCirculation = this.totalAmountInCirculation.get();
+    this.totalAmountInCirculation.assertEquals(totalAmountInCirculation);
+
+    let newTotalAmountInCirculation = totalAmountInCirculation.add(amount);
+    let nonce = this.mintNonce.get();
+    this.mintNonce.assertEquals(nonce);
+
+    adminSignature
+      .verify(
+        this.address,
+        amount
+          .toFields()
+          .concat(...receiverAddress.toFields(), ...nonce.toFields())
+      )
+      .assertTrue();
+
+    this.token.mint({
+      address: receiverAddress,
+      amount,
+    });
+
+    this.mintNonce.set(nonce.add(1));
+    this.totalAmountInCirculation.set(newTotalAmountInCirculation);
+  }
+
+  @method sendTokens(
+    senderAddress: PublicKey,
+    receiverAddress: PublicKey,
+    amount: UInt64
+  ) {
+    this.token.send({
+      from: senderAddress,
+      to: receiverAddress,
+      amount,
+    });
+  }
+}
