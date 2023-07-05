236a3126bc2338b447c7b76d8ea3aba9fb3ed1dd
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2023-07-05 15:34:52 +0530

    feat: created instance of zkApp

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -2,31 +2,30 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { useEffect } from 'react';
-import GradientBG from '../components/GradientBG.js';
 import styles from '../styles/Home.module.css';
-import MyForm from '@/components/Form';
 import Seat from '@/components/Seat';
 import Header from '@/components/Header';
 
 export default function Home() {
-  // useEffect(() => {
-  //   (async () => {
-  //     const { Mina, PublicKey } = await import('snarkyjs');
-  //     // const { Add } = await import('../../../contracts/build/src/');
+  useEffect(() => {
+    (async () => {
+      const { Mina, PublicKey } = await import('snarkyjs');
+      const { Token } = await import('../../../contracts/build/src/Token');
 
-  //     // Update this to use the address (public key) for your zkApp account.
-  //     // To try it out, you can try this address for an example "Add" smart contract that we've deployed to
-  //     // Berkeley Testnet B62qkwohsqTBPsvhYE8cPZSpzJMgoKn4i1LQRuBAtVXWpaT4dgH6WoA.
-  //     const zkAppAddress = '';
-  //     // This should be removed once the zkAppAddress is updated.
-  //     if (!zkAppAddress) {
-  //       console.error(
-  //         'The following error is caused because the zkAppAddress has an empty string as the public key. Update the zkAppAddress with the public key for your zkApp account, or try this address for an example "Add" smart contract that we deployed to Berkeley Testnet: B62qkwohsqTBPsvhYE8cPZSpzJMgoKn4i1LQRuBAtVXWpaT4dgH6WoA'
-  //       );
-  //     }
-  //     //const zkApp = new Add(PublicKey.fromBase58(zkAppAddress))
-  //   })();
-  // }, []);
+      // Update this to use the address (public key) for your zkApp account.
+      // To try it out, you can try this address for an example "Add" smart contract that we've deployed to
+      // Berkeley Testnet B62qkwohsqTBPsvhYE8cPZSpzJMgoKn4i1LQRuBAtVXWpaT4dgH6WoA.
+      const zkAppAddress = 'B62qmhRvvHx8Pf4uZfDTUn966V8ZJSRx8MpTgaJydBAjzSasHm9u92J';
+      // This should be removed once the zkAppAddress is updated.
+      if (!zkAppAddress) {
+        console.error(
+          'The following error is caused because the zkAppAddress has an empty string as the public key. Update the zkAppAddress with the public key for your zkApp account, or try this address for an example "Add" smart contract that we deployed to Berkeley Testnet: B62qkwohsqTBPsvhYE8cPZSpzJMgoKn4i1LQRuBAtVXWpaT4dgH6WoA'
+        );
+      }
+      const zkApp = new Token(PublicKey.fromBase58(zkAppAddress))
+      console.log('zkApp', zkApp);
+    })();
+  }, []);
 
   return (
     <>
---
+++ ui/src/pages/zkappWorker.ts
@@ -0,0 +1,88 @@
+import { Mina, isReady, PublicKey, fetchAccount } from 'snarkyjs';
+
+type Transaction = Awaited<ReturnType<typeof Mina.transaction>>;
+
+// ---------------------------------------------------------------------------------------
+
+import type { Token } from '../../../contracts/src/Token';
+
+const state = {
+  Token: null as null | typeof Token,
+  zkapp: null as null | Token,
+  transaction: null as null | Transaction,
+};
+
+// ---------------------------------------------------------------------------------------
+
+const functions = {
+  loadSnarkyJS: async (args: {}) => {
+    await isReady;
+  },
+  setActiveInstanceToBerkeley: async (args: {}) => {
+    const Berkeley = Mina.Network(
+      'https://proxy.berkeley.minaexplorer.com/graphql'
+    );
+    Mina.setActiveInstance(Berkeley);
+  },
+  loadContract: async (args: {}) => {
+    const { Token } = await import('../../../contracts/build/src/Token');
+    state.Token = Token;
+  },
+  compileContract: async (args: {}) => {
+    await state.Token!.compile();
+  },
+  fetchAccount: async (args: { publicKey58: string }) => {
+    const publicKey = PublicKey.fromBase58(args.publicKey58);
+    return await fetchAccount({ publicKey });
+  },
+  initZkappInstance: async (args: { publicKey58: string }) => {
+    const publicKey = PublicKey.fromBase58(args.publicKey58);
+    state.zkapp = new state.Token!(publicKey);
+  },
+//   mintTokens:
+//   getTokenAmount: async (args: {account: state.token.id}) => {
+//     const currentTokens = await Mina.getBalance( account , state.zkapp!.address);
+//     return JSON.stringify(currentNum.toJSON());
+//   },
+//   createUpdateTransaction: async (args: {}) => {
+//     const transaction = await Mina.transaction(() => {
+//       state.zkapp!.update();
+//     });
+//     state.transaction = transaction;
+//   },
+  proveUpdateTransaction: async (args: {}) => {
+    await state.transaction!.prove();
+  },
+  getTransactionJSON: async (args: {}) => {
+    return state.transaction!.toJSON();
+  },
+};
+
+// ---------------------------------------------------------------------------------------
+
+export type WorkerFunctions = keyof typeof functions;
+
+export type ZkappWorkerRequest = {
+  id: number;
+  fn: WorkerFunctions;
+  args: any;
+};
+
+export type ZkappWorkerReponse = {
+  id: number;
+  data: any;
+};
+if (process.browser) {
+  addEventListener(
+    'message',
+    async (event: MessageEvent<ZkappWorkerRequest>) => {
+      const returnData = await functions[event.data.fn](event.data.args);
+
+      const message: ZkappWorkerReponse = {
+        id: event.data.id,
+        data: returnData,
+      };
+      postMessage(message);
+    }
+  );
+}
